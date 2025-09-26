3f6337c7fa9badf308384afb665d17c5e3507a05
Harshita Tripathi <148742116+HarshitaTripathii@users.noreply.github.com>
2025-09-26 11:54:47 +0000

    trail : workspaces

---
+++ react/trail1/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className="underline decoration-red-100 text-red-50 bg-blue-700">
+      harshita is great !!
+     </h1>
+    </>
+  )
+}
+
+export default App
